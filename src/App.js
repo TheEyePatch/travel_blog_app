c74a2d514995bc636b76c5cbc2aedb8886ce8460
Leif Bersamina <leifbersamina@gmail.com>
2021-08-14 23:22:37 +0800

    wrap App inside MainNavigation component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom'
 import './App.css';
+import NewTravelBlog from './pages/NewTravelBlog';
+import TravelBlogs from './pages/TravelBlogs';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MainNavigation/>
+      <Switch>
+        <Route path='/' exact>
+          <TravelBlogs/>
+        </Route>
+        <Route path='/travel_blogs/new' exact>
+          <NewTravelBlog/>
+        </Route>
+      </Switch>
+      
     </div>
   );
 }
