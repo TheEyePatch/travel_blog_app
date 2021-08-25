949737009717ce110177741ac2ec2f8d2515fb90
Leif Bersamina <leifbersamina@gmail.com>
2021-08-25 19:53:14 +0800

    add sign out button and conditional routing

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -4,16 +4,23 @@ import classes from './MainNavigation.module.css'
 import AuthContext from '../../contexts/auth-context';
 function MainNavigation(){
   const authContext = useContext(AuthContext);
-
+  const userLoggedIn = authContext.userLoggedIn;
   return (
     <header className={classes.header}>
       <h1 className={classes.logo}>Travel Blog</h1>
       <nav className={classes.nav}>
         <ul className={classes.link_list}>
+          
           <li><Link to='/' >Travel Blogs</Link></li>
-          <li><Link to = '/travel_blogs/new'>New Blog</Link></li>
+          {userLoggedIn && 
+            <li><Link to = '/travel_blogs/new'>New Blog</Link></li>
+          }
+          { userLoggedIn &&
+            <li><button onClick={authContext.logout}  >Sign Out</button></li> 
+            }
+          {!userLoggedIn && 
           <li><Link to = '/sign_in'>Sign In</Link></li>
-          <li onClick={authContext.logout} ><Link >Sign Out</Link></li>
+          }
         </ul>
       </nav>
     </header>
