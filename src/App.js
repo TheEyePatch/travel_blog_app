e2b9ca8528b68d67c1672ae292ac56f580f62ac9
Leif Bersamina <leifbersamina@gmail.com>
2021-08-25 19:52:05 +0800

    Limit routes for user authorization

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import { useContext } from 'react';
 import './App.css';
 import NewTravelBlog from './pages/NewTravelBlog';
 import TravelBlogs from './pages/TravelBlogs';
+import TravelBlog from './pages/TravelBlog';
 import MainNavigation from './components/layout/MainNavigation';
 import SignIn from './pages/SignIn';
+import { useContext } from 'react';
+import AuthContext from './contexts/auth-context';
+import { BlogContextProvider } from './contexts/blog-context'
+import BlogContext from './contexts/blog-context';
 
 function App() {
+  const authContext = useContext(AuthContext)
+  const userLoggedIn = authContext.userLoggedIn;
+  const blogContext = useContext(BlogContext);
+
   return (
     <div>
       <MainNavigation/>
@@ -13,14 +23,34 @@ function App() {
         <Route path='/' exact>
           <TravelBlogs/>
         </Route>
-        <Route path='/travel_blogs/new' exact>
-          <NewTravelBlog/>
-        </Route>
-        <Route path='/sign_in' exact>
-          <SignIn/>
+        {
+          userLoggedIn && (
+          <Route path='/travel_blogs/new' exact>
+            <NewTravelBlog/>
+          </Route>
+          )
+        }
+
+        {
+          userLoggedIn && (
+          <BlogContextProvider>
+            <Route path= {`/travel_blogs/${blogContext.blog_id}`}>
+              <TravelBlog/>
+            </Route>
+          </BlogContextProvider>
+            
+          )
+        }
+        
+        { !userLoggedIn && 
+          <Route path='/sign_in' exact>
+            <SignIn/>
+          </Route>
+        }
+        <Route path='*'>
+          <Redirect to='/' />
         </Route>
       </Switch>
-      
     </div>
   );
 }
