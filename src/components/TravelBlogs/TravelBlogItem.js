975452c5512d9750c70c615b0d96486ef130f433
Leif Bersamina <leifbersamina@gmail.com>
2021-08-25 19:51:32 +0800

    modify props

--- src/components/TravelBlogs/TravelBlogItem.js
+++ src/components/TravelBlogs/TravelBlogItem.js
@@ -1,12 +1,14 @@
 import Card from '../Card'
+import {Link} from 'react-router-dom'
 
 function TravelBlogItem(props){
+  const blog = props.object;
   return (  
     <div>
       <Card>
         <div className={props.className}>
-          <h1>{props.title}</h1>
-          <p>{props.description}</p>
+          <h1>{blog.title}</h1>
+          <p>{blog.description}</p>
         </div>
       </Card>
     </div>
