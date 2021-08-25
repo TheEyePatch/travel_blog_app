6de6c21a1324c8496296c8722c2d3507e014fba9
Leif Bersamina <leifbersamina@gmail.com>
2021-08-26 02:09:58 +0800

    add useEffect for GET request

--- src/pages/TravelBlog.js
+++ src/pages/TravelBlog.js
@@ -1,5 +1,26 @@
-const TravelBlog = (props) => {
-  
+import { useEffect, useState } from 'react';
+import { useParams} from "react-router-dom";
+import classes from './styles/TravelBlog.module.css'
+
+const TravelBlog = () => {
+  const params = useParams();
+  const [blog, setBlog] = useState({});
+  useEffect(() => {
+    fetch(`http://localhost:3001/blogs/${params.id}`,{
+      method: 'GET',
+      'Content-Type': 'application/json'
+    }).then(data => data.json())
+    .then(data => setBlog(data) )
+  }, [])
+
+
+  return (
+    <section className={classes.section}>
+      <h1>{blog.id}</h1>
+      <h1>{blog.title}</h1>
+      <p>{blog.description}</p>
+    </section> 
+  ) 
 };
 
 
