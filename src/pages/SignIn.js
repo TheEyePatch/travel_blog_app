8994dfd1fd63f5dd3baa60aea9e74c66da6e2a57
Leif Bersamina <leifbersamina@gmail.com>
2021-08-26 20:41:02 +0800

    add condition for ModalBackground and UserRegistrationForm render

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,9 +1,27 @@
 import classes from './styles/SignIn.module.css'
 import SignInForm from '../components/Forms/SignInForm'
+import UserRegistrationForm from '../components/Forms/UserRegistrationForm'
+import ModalBackground from '../components/ModalBackground'
+import { useState } from 'react'
 function SignIn(){
+  const [register, setRegister] = useState(false);
+
+  const registerHandler = (event) =>{
+    event.preventDefault();
+    if(!register){
+      setRegister(true)
+    }else{
+      setRegister(false)
+    }
+    
+  }
+
   return (
     <section>
-      <SignInForm/>
+      <SignInForm onSet={registerHandler}/>
+      {register && <ModalBackground setRegister={registerHandler}/>}
+      {register && <UserRegistrationForm/>}
+      
     </section>
   )
 }
