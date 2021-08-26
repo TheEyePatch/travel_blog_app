efc96cd0f25b9f134fcb34d16c1a83c21881908a
Leif Bersamina <leifbersamina@gmail.com>
2021-08-26 20:40:30 +0800

    add useEffect and fetch for TravelBlog

--- src/pages/TravelBlog.js
+++ src/pages/TravelBlog.js
@@ -4,21 +4,34 @@ import classes from './styles/TravelBlog.module.css'
 
 const TravelBlog = () => {
   const params = useParams();
-  const [blog, setBlog] = useState({});
+  const [object, setBlog] = useState({});
+  const [isLoading, setLoading] = useState(true);
+
   useEffect(() => {
     fetch(`http://localhost:3001/blogs/${params.id}`,{
       method: 'GET',
       'Content-Type': 'application/json'
     }).then(data => data.json())
-    .then(data => setBlog(data) )
-  }, [])
+    .then(data =>{
+      setLoading(false)
+      setBlog({ author: data.author, ...data.blog}) 
+    } )
+  }, [params.id])
 
+  console.log(object)
+  console.log(params.id)
 
+  if(isLoading === true){
+    return(<section>
+      <p>Loading...</p>
+    </section>
+    );
+  }
   return (
     <section className={classes.section}>
-      <h1>{blog.id}</h1>
-      <h1>{blog.title}</h1>
-      <p>{blog.description}</p>
+      <h1>{object.title}</h1>
+      <h1>{object.description}</h1>
+      <h1>{object.author}</h1>
     </section> 
   ) 
 };
