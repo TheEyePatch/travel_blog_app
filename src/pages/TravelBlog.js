ad6513437583dca003c2d544f2ee1fafc0d172ff
Leif Bersamina <leifbersamina@gmail.com>
2021-08-28 22:05:42 +0800

    wrap TravelBlogItem inside UserContextProvider

--- src/pages/TravelBlog.js
+++ src/pages/TravelBlog.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import { useParams} from "react-router-dom";
+import TravelBlogItem from '../components/TravelBlogs/TravelBlogItem';
 import classes from './styles/TravelBlog.module.css'
+import { UserContextProvider } from '../contexts/user-context';
+
 
 const TravelBlog = () => {
   const params = useParams();
@@ -18,9 +21,6 @@ const TravelBlog = () => {
     } )
   }, [params.id])
 
-  console.log(object)
-  console.log(params.id)
-
   if(isLoading === true){
     return(<section>
       <p>Loading...</p>
@@ -29,11 +29,11 @@ const TravelBlog = () => {
   }
   return (
     <section className={classes.section}>
-      <h1>{object.title}</h1>
-      <h1>{object.description}</h1>
-      <h1>{object.author}</h1>
-    </section> 
-  ) 
+      <UserContextProvider>
+        <TravelBlogItem object={object}/> 
+      </UserContextProvider>
+    </section>
+  )
 };
 
 
