f31ab770e02e3f23152ff2a4c1759f652e6d7153
Leif Bersamina <leifbersamina@gmail.com>
2021-08-28 22:07:21 +0800

    add useContext for UserContext

--- src/components/TravelBlogs/TravelBlogItem.js
+++ src/components/TravelBlogs/TravelBlogItem.js
@@ -1,14 +1,43 @@
 import Card from '../Card'
-import {Link} from 'react-router-dom'
+import {Link, Route, Switch} from 'react-router-dom'
+import { useContext } from 'react';
+import UserContext from '../../contexts/user-context';
+import AuthContext from '../../contexts/auth-context';
 
 function TravelBlogItem(props){
   const blog = props.object;
+  const user_context = useContext(UserContext);
+  const authContext = useContext(AuthContext)
+  const userLoggedIn = authContext.userLoggedIn;
+  if(userLoggedIn){
+    user_context.getUser();
+  }
+  
+  const isAuthor = user_context.email == blog.author
+
+  const header = (
+    <Switch>
+      <Route path='/' exact>
+      <Link to={`/travel_blogs/${blog.id}`}><h1>{blog.title}</h1></Link>
+      </Route>
+      <Route path='*'>
+      <h1>{blog.title}</h1>
+      </Route>
+    </Switch>
+  )
+
   return (  
     <div>
       <Card>
         <div className={props.className}>
-          <Link to={`/travel_blogs/${blog.id}`}><h1>{blog.title}</h1></Link>
+          {header}
           <p>{blog.description}</p>
+          <p>{blog.author}</p>
+
+          <Route path='/travel_blogs/:id' exact>
+            {isAuthor && userLoggedIn && <button>Update</button>}
+          </Route>
+
         </div>
       </Card>
     </div>
